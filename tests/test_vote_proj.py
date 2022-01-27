56efbb9198f39e53ef534943a94f15d18b38ca3f
omanduj <omanduj@gmail.com>
2022-01-26 23:19:21 -0600

    starting tests

--- tests/test_vote_proj.py
+++ tests/test_vote_proj.py
@@ -1,5 +1,9 @@
 from vote_proj import __version__
-
+from vote_proj.user.models import get_email
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_email():
+    session["user"]["email"] = '123@123.com'
+    assert get_email() == '123@123.com'
